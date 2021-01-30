9b8908fb4e47ca871e7bbc169b08fc8540125ec4
Nozomi Mitsuo <ximei816@gmail.com>
2021-01-30 21:48:34 +0800

    Initial Commit

---
+++ TaxiNearMe_SG/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaxiNearMe_SG
+//
+//  Created by 満尾希美 on 30/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaxiNearMe_SG/TaxiNearMe_SGApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiNearMe_SGApp.swift
+//  TaxiNearMe_SG
+//
+//  Created by 満尾希美 on 30/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TaxiNearMe_SGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
